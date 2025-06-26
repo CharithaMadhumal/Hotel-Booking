25b6bbb239ac8bbca4d6364057c57249b33f2a71
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-06-26 22:34:06 +0530

    done room-details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Navbar from './components/NavBar'
-import { useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
+import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
+
+
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
-     {!isOwnerPath && <Navbar />}
+      {!isOwnerPath && <Navbar />}
+
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+
+        </Routes>
+
+      </div>
     </div>
   )
 }
