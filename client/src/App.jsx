dd5b3890a7da27ebf119893033baadff5c439c72
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-07-08 00:07:16 +0530

    done dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import Footer from './components/Footer';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 
 
@@ -15,6 +20,7 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
 
       <div className='min-h-[70vh]'>
         <Routes>
@@ -22,7 +28,12 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path='/my-bookings' element={<MyBookings />}/>
+          <Route path='/owner' element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path='add-room' element={<AddRoom/>}/>
+              <Route path='list-room' element={<ListRoom/>}/>
 
+          </Route>
           
 
         </Routes>
